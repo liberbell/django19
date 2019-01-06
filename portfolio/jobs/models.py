393f5a39bc6d4eaed889a7d73c250e29d584b8cc
liberbell <liberbell@gmail.com>
2019-01-07 07:46:51 +0900

    models3

--- portfolio/jobs/models.py
+++ portfolio/jobs/models.py
@@ -5,4 +5,4 @@ class Job(models.Model):
     # Images
     image = models.ImageField(upload_to='images/')
     # Summary
-    summary = models.CharField()
+    summary = models.CharField(max_length=200)
