a972258567839ad59171b55fcfa9e0ec48825de2
liberbell <liberbell@gmail.com>
2019-01-09 07:55:04 +0900

    views1

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def nick(request):
     return render(request, 'jobs/home.html')
+
+def homepage(request):
+    return render(request, 'jobs/home.html')
