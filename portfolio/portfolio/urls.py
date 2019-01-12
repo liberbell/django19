6f76fccd01d8dfe9791fecb17ac8c60f43704f96
liberbell <liberbell@gmail.com>
2019-01-12 18:31:24 +0900

    urls1

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import jobs.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('nick', jobs.views.nick, name='nick'),
     path('', jobs.views.homepage, name='nick'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
