4429daf504dab43f53faccf0bc324a9f7d00c464
liberbell <liberbell@gmail.com>
2019-01-14 14:19:50 +0900

    urls1

--- portfolio/portfolio/urls.py
+++ portfolio/portfolio/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # path('nick', jobs.views.nick, name='nick'),
     path('', jobs.views.homepage, name='nick'),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
