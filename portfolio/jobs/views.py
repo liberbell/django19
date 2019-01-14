c4793418408cd3f62c0c9b8fb3a840de76a9e658
liberbell <liberbell@gmail.com>
2019-01-14 14:48:06 +0900

    views3

--- portfolio/jobs/views.py
+++ portfolio/jobs/views.py
@@ -8,3 +8,7 @@ def nick(request):
 def homepage(request):
     jobs = Job.objects
     return render(request, 'jobs/home.html', {'jobs': jobs})
+
+def detail(request, job_id):
+    print(job_id)
+    return render(request, 'jobs/home.html')
